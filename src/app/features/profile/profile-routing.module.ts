ff775f3b6cb3ee9f0f1cb57adbbb552cc9643af0
NikitaT1 <nik.tati1@gmail.com>
2021-07-15 23:55:49 +0300

    Refactoring

--- src/app/features/profile/profile-routing.module.ts
+++ src/app/features/profile/profile-routing.module.ts
@@ -1,17 +1,7 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { ProfileComponent } from './profile.component';
-
-const routes: Routes = [
-  {
-    path: 'profile',
-    component: ProfileComponent
-  },
-];
-
+import { RouterModule } from '@angular/router';
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule],
   exports: [RouterModule]
 })
 export class ProfileRoutingModule { }
