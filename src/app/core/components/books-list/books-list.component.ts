26a2cac56fc36ded38473ad4ae5d5b884afb8e52
NikitaT1 <nik.tati1@gmail.com>
2021-07-16 14:53:43 +0300

    Updated mainPage view, also add some media queries just for testing

--- src/app/core/components/books-list/books-list.component.ts
+++ src/app/core/components/books-list/books-list.component.ts
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksListComponent {
   title = 'Title';
-  books = ['book1', 'book2', 'book3'];
-  author = ['author1', 'author', 'author'];
-  genres = ['genre1', 'genre2', 'genre3'];
-  rating = ['1', '2', '3'];
-  
+  books = ['book1', 'book2', 'book3', 'book4'];
+  authors = ['author1', 'author2', 'author3', 'author4'];
+  genres = ['genre1', 'genre2', 'genre3', 'genre4'];
+  rating = [1, 2, 3, 4];
 }
