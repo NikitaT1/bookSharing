33a06d33f2c665d4660b25ee0290b8bf4d8f6b12
NikitaT1 <nik.tati1@gmail.com>
2021-07-20 14:19:14 +0300

    Transfered books-list component into feature folder, also added homepage module there
    Refactoring of homepage module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BooksListComponent } from './components/books-list/books-list.component';
+import { BooksListComponent } from '../features/homepage/books-list/books-list.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
