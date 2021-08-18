b5ac6b0474267b63b45c866e5a8355a21b015090
NikitaT1 <50304586+NikitaT1@users.noreply.github.com>
2021-08-18 11:51:45 +0300

    Int 2.header.user menu (#11)
    * INT-2.Header.User-menu styling
    
    * INT-2.Header. Add styling
    
    * INT-2.Header. Added avatar component and module
    
    * INT-2.Header.More styling
    
    * INT-2.Header.Restyling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AddButtonModule } from './shared/components/add-button/add-button.module';
-import { SearchModule } from './shared/components/search/search.module';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderModule } from './core/components/header/header.module';
--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   notifications = 0;
+
+  choosePoint(){
+    console.log('bum!')
+  }
 }
+ 
