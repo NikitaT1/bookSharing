a5c5f3dd24ee31763965a43ad80670182d3d9985
NikitaT1 <nik.tati1@gmail.com>
2021-08-31 13:13:39 +0300

    INT-6.Homepage.Details text styiling

--- src/app/features/homepage/details/details.component.ts
+++ src/app/features/homepage/details/details.component.ts
@@ -13,6 +13,7 @@ export class DetailsComponent  {
     name: 'The Book of Kells',
     author: 'Unknown',
     genre: 'Non-fiction',
+    shortDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     rating: 8,
     isFavorite: true,
@@ -23,6 +24,11 @@ export class DetailsComponent  {
   detailsStar = new Array(this.details.rating - 1)
   detailsNoStar = new Array(10 - this.details.rating)
 
+  hide = true
+
+  showHideDetails(){
+    this.hide = !this.hide
+  }
 
   OnInit(): void {
   }
